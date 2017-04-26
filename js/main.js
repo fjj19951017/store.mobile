3409133786de6174273566295d9b885603025501
fjj19951017 <948204381@qq.com>
2017-04-26 20:40:47 +0800

    2017/4/26

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2017/3/16.
+ */
+$(function() {
+
+    $('.carousel').carousel('cycle');
+
+    $('.icon:eq(0)').click(function() {
+        window.location = 'html/user.html';
+    });
+
+    $('.icon:eq(1)').click(function() {
+        window.location = 'html/user.html';
+    });
+
+    $('.icon:eq(2)').click(function() {
+        window.location = 'html/login.html';
+    });
+
+    $('.icon:eq(3)').click(function() {
+        window.location = 'html/user.html';
+    });
+})
